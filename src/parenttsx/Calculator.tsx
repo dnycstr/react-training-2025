51936badc615099ea4c55d7a785412a178f661e3
JiEstrope <darleeneestrope7@gmail.com>
2025-03-05 19:23:31 +0800

    Calculator

--- src/parenttsx/Calculator.tsx
+++ src/parenttsx/Calculator.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 export const Calculator = () => { 
     const [value, setValue] = useState<number>(0);
     const [value2, setValue2] = useState<number>(0);
@@ -67,42 +68,46 @@ export const Calculator = () => {
 
     return (
         <>
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "20px" }}>
+    
+
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "50px" }}>
                 <h1>Calculator</h1>
                 
-                <div style={{ display: "flex", border: "solid black 2px", flexDirection: "row", padding: "10px 20px 10px 20px", gap: "8px"}}>
-                    <div>{value}</div>
-                    {operation && <div>{operation}</div>}
-                    <div>{value2}</div> = <div style={{ fontWeight: "bold" }}>{total}</div>
-                </div>
-                <button onClick={handleClear}>Clear</button>
-                    <button onClick={() => handleClick(0)}>0</button>
-                    <button onClick={() => handleClick(1)}>1</button>
-                    <button onClick={() => handleClick(2)}>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                <button onClick={() => handleOperation("+")}>+</button>
-                <button onClick={() => handleOperation("-")}>-</button>
-                <button onClick={() => handleOperation("/")}>/</button>
-                <button onClick={() => handleOperation("*")}>x</button>
-                <button onClick={() => handleOperationClick()}>=</button>
-
-                <div style={{ display: "flex", gap: "5px", flexDirection: "row", padding: "10px 20px" }}>
-                <button onClick={() => handleClick(1)}>1</button>
-                    <button onClick={() => handleClick(2)}>2</button>
-                    <button>3</button>
+                <div style={{ display: "flex", border: "solid black 2px", flexDirection: "row", padding: "10px 20px 10px 20px", gap: "30px" }}>
+                    
+                    <div style={{ fontWeight: "bold", fontSize: "30px" }}>{value}</div>
+                    {operation && <div style={{ fontWeight: "bold", fontSize: "30px" }}>{operation}</div>}
+                    <div style={{ fontWeight: "bold", fontSize: "30px" }}>{value2}</div> = <div style={{ fontWeight: "bold", fontSize: "30px" }}>{total}</div>
                 </div>
 
-                <div style={{ display: "flex", gap: "5px", flexDirection: "row", padding: "10px 20px" }}>
-                <button onClick={() => handleClick(4)}>4</button>
-                    <button onClick={() => handleClick(5)}>5</button>
-                    <button>6</button>
+                <div className="button-calc-container">
+                    <button className="button-calc" onClick={() => handleClick(1)}>1</button>
+                    <button className="button-calc" onClick={() => handleClick(2)}>2</button>
+                    <button className="button-calc"  onClick={() => handleClick(3)}>3</button>  
+                    <button className="button-calc" onClick={handleClear}>C</button>
                 </div>
+
+                <div className="button-calc-container">
+                <button className="button-calc" onClick={() => handleClick(4)}>4</button>
+                <button className="button-calc" onClick={() => handleClick(5)}>5</button>
+                <button className="button-calc" onClick={() => handleClick(6)}>6</button>
+                <button className="button-calc" onClick={() => handleOperationClick()}>=</button>
+                </div>
+
+                <div className="button-calc-container">
+                    <button className="button-calc" onClick={() => handleClick(7)}>7</button>
+                    <button className="button-calc" onClick={() => handleClick(8)}>8</button>
+                    <button className="button-calc" onClick={() => handleClick(9)}>9</button>
+                    <button className="button-calc" onClick={() => handleClick(0)}>0</button>
+                </div>
+
+                <div className="button-calc-container">
+                <button className="button-calc" onClick={() => handleOperation("+")}>+</button>
+                <button className="button-calc" onClick={() => handleOperation("-")}>-</button>
+                <button className="button-calc" onClick={() => handleOperation("/")}>/</button>
+                <button className="button-calc" onClick={() => handleOperation("*")}>*</button>
+                </div>
+
         </div>
 
         
